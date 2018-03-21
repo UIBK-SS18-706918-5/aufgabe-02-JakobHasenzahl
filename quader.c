80785aaba6eebdda5b3e25b88feb28eaecfe1c30
Jakob Hasenzahl <csav2973@zid-t9pc1920.uibk.ac.at>
2018-03-21 15:21:56 +0100

    Teilaufgabe b

--- quader.c
+++ quader.c
@@ -2,22 +2,30 @@
 #include <stdlib.h>
 
 int main(int argc, char ** argv) {
-	float a, b, c;
+	float a;
+	float b;
+	float c;
 
-	// Einlesen der Seitenlängen
-	a = atof(argv[1]);
-	b = atof(argv[2]);
-	c = atof(argv[3]);
+	//printf("%i \n",argc);
 
-	// Berechnung der Oberfläche
-	float oberflaeche = 0;
-	oberflaeche += 2.*a*b;
-	oberflaeche += 2.*a*c;
-	oberflaeche += 2.*b*c;
+	if(argc != 4)
+	{	
+		printf("Something went wrong with your input, please check again!\n");
+	}
+	else
+	{
+		a = atof(argv[1]);
+		b = atof(argv[2]);
+		c = atof(argv[3]);
+		// Berechnung der Oberfläche
+		float oberflaeche = 0;
+		oberflaeche += 2.*a*b;
+		oberflaeche += 2.*a*c;
+		oberflaeche += 2.*b*c;
 
-	// Ausgabe
-	printf("Ein Quader mit den Seitenlängen %f, %f und %f hat die Oberfläche %f\n",
-		a, b, c, oberflaeche);
+		// Ausgabe
+		printf("Ein Quader mit den Seitenlängen %f, %f und %f hat die Oberfläche %f\n",a, b, c, oberflaeche);
+	}
 
 	return 0;
 }
